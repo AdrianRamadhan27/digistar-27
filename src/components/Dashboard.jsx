907379ec700fad5dc54d81742cbff5707756834a
Raden Mohamad Adrian Ramadhan Hendar Wibawa <raden.mohamad11@ui.ac.id>
2024-09-07 10:57:38 +0700

    style: lighten gradient colors

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
                 <button className="text-white bg-accent hover:bg-accent-hover rounded-b-md p-2 text-sm font-semibold">Export to PDF</button>
             </div>
             <div className="p-5 mt-5">
-                <div className="bg-gradient-to-r from-white via-pink-600/75 to-accent/75 bg-opacity-50 flex justify-between rounded-md w-full p-5 h-40">
+                <div className="bg-gradient-to-r from-white via-pink-400/50 to-accent/50 bg-opacity-50 flex justify-between rounded-md w-full p-5 h-40">
                     <div className="flex flex-col text-left gap-3">
                         <img src={import.meta.env.BASE_URL+"/images/logo.png"} alt="" className="w-32"/>
                         <div className="w-fit rounded-full p-1 text-white bg-accent text-xs font-semibold">OCA Interaction</div>

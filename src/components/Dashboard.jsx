a4d33350381d889bf29ac467e958980e1a69da91
Raden Mohamad Adrian Ramadhan Hendar Wibawa <raden.mohamad11@ui.ac.id>
2024-09-07 10:54:10 +0700

    style: semibolden text & gradient opacity

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,13 +5,13 @@ export default function Dashboard() {
     return (
         <div className="w-full">
             <div className="text-right px-5">
-                <button className="text-white bg-accent hover:bg-accent-hover rounded-b-md p-2 text-sm">Export to PDF</button>
+                <button className="text-white bg-accent hover:bg-accent-hover rounded-b-md p-2 text-sm font-semibold">Export to PDF</button>
             </div>
             <div className="p-5 mt-5">
-                <div className="bg-gradient-to-r from-white via-pink-600 to-accent flex justify-between rounded-md w-full p-5 h-40">
+                <div className="bg-gradient-to-r from-white via-pink-600/75 to-accent/75 bg-opacity-50 flex justify-between rounded-md w-full p-5 h-40">
                     <div className="flex flex-col text-left gap-3">
                         <img src={import.meta.env.BASE_URL+"/images/logo.png"} alt="" className="w-32"/>
-                        <div className="w-fit rounded-full p-1 text-white bg-accent text-xs">OCA Interaction</div>
+                        <div className="w-fit rounded-full p-1 text-white bg-accent text-xs font-semibold">OCA Interaction</div>
                         <h1 className="text-accent font-extrabold text-2xl">Data Visualization Dashboard</h1>
                     </div>
                     <img src={import.meta.env.BASE_URL+"/images/cover.png"} alt=""/>
